942d511b7a447213062ac6ee84e60c0293abbe43
marcionitao <marcionitao@gmail.com>
2017-12-09 23:55:49 +0100

    create product model

---
+++ src/models/product.js
@@ -0,0 +1,39 @@
+// temos de criar um schema, no MongoDB o shema é criado na aplicação e não na DB
+"use strict";
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    title: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    slug: {
+        type: String,
+        require: true,
+        trim: true,
+        index: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        require: true
+    },
+    active: {
+        type: Boolean,
+        require: true,
+        default: true
+    },
+    tags: [{
+        type: String,
+        require: true
+    }]
+});
+
+module.exports = mongoose.model('Product',schema);
