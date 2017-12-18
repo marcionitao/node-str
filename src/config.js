11e54cb74beb22b8d0a1eb7a3fa064cc01eed960
marcionitao <marcionitao@gmail.com>
2017-12-18 09:07:29 +0100

    hide configurations

--- src/config.js
+++
@@ -1,8 +0,0 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo a Node Store!';
-
-module.exports = {
-    connectionString: 'mongodb://balta:balta@ds044787.mlab.com:44787/ndstr', // mlab
-    sendgridKey: '', 
-    constainerConnectionString: 'TBO'
-};
